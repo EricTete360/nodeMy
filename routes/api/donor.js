dded86fbf9f42ead3dab1ecf64734abb88cc06ca
EricTete360 <duckworth578@gmail.com>
2020-07-27 19:51:42 +0530

    file module changed

--- routes/api/donor.js
+++ routes/api/donor.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
-const fileUpload = require('express-fileupload');
+
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
-const fileUpload = require('express-fileupload');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
